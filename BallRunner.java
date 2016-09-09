88328ab2b6c0c96c0c2af2037ca99fc287cbee1a
790036 <790036@seq.org>
2016-09-09 08:33:06 -0700

    Add files via upload

--- BallRunner.java
+++ BallRunner.java
@@ -7,9 +7,11 @@
  */
 public class BallRunner
 {
-    // instance variables - replace the example below with your own
-    private int x;
-    private int heading;
+    BallWorld ballWorld = new BallWorld(500, 500);
+    TGPoint entrancePoint = new TGPoint(0,0);
+    BallBot ballBotArray[] = new BallBot[12];//(ballWorld, entrancePoint, 0, 25)
+   
+    private double heading;
     //private TGPoint gat = (0,0);
     /**
      * Constructor for objects of class BallRunner
@@ -17,20 +19,42 @@ public class BallRunner
     public BallRunner()
     {
         // initialise instance variables
-        x = 0;
+        BallWorld ballWorld = new BallWorld(500,500);
+        TGPoint entrancePoint = new TGPoint(0,0);
+        int ballBotArrayLength = 12;
+
     }
 
-    public static void run(){
-       BallWorld ballWorld = new BallWorld(200, 200);
-       TGPoint tgPoint = new TGPoint(0,0);
-       BallBot ballBot = new BallBot(ballWorld, tgPoint, 0, 25);
-       while(x=0){
-           if(ballBot.canMoveForward = true){
-               ballBot.moveForward;
+    public static void activity1(){
+      int x = 0;
+       BallWorld ballWorld1 = new BallWorld(200, 200);
+       TGPoint tgPoint1 = new TGPoint(0,0);
+       BallBot ballBot1 = new BallBot(ballWorld1, tgPoint1, 0, 25);
+       while(x==0){
+           if(ballBot1.canMoveForward(ballWorld1) == true){
+               ballBot1.moveForward();
             }
            else{
-               heading = ballBot.getHeading();
-               ballBot.setHeading((heading%360)+90);
+               ballBot1.setHeading((ballBot1.getHeading()%360)+90);
             }
         }
+    }
+    public int findFreeBallBotIndex(){
+        int gat = 0;
+        for(int i = 0; i < ballBotArray.length; i++){
+            if(ballBotArray[i] == null){
+                gat = i;
+            }
+            else{
+                gat = ballBotArray.length;
+            
+            }
+            }
+        return gat;
+    }    
+    public static void activity2(){
+        
     }}
+
+
+
